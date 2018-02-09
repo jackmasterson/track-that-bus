86bded7f485b0cd91041aa59d9b0d2820f2b740f
Jack Masterson <jmasterson@cafemedia.com>
2018-02-09 14:50:54 -0500

    add border to selection

--- src/components/SelectionScreen.js
+++ src/components/SelectionScreen.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
 export class SelectionScreen extends Component {
+    selectDestination(val, e) {
+        let d = e.target;
+        d.style.border = '1px solid black';
+        let divs = document.querySelectorAll('.selection');
+        for (let div of divs) {
+            div.style.border = '';
+        }
+        this.props.selectDestination(val);
+        
+    }
     render() {
         return (
             <div>
                 <div><h2>Destinations</h2>
                     {this.options.destinations.map((dest, incr) => {
                         return (
-                            <div key={incr} onClick={() => this.props.selectDestination(dest)}>{dest.dest}</div>
+                            <div className="selection" key={incr} onClick={(e) => this.selectDestination(dest, e)}>{dest.dest}</div>
                         );
                     })}
                 </div>
